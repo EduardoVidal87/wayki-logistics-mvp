e7ccd9d675c9c0e80daea2cd0eac937dd91dc0eb
EduardoVidal87 <eduardo@wayki.co>
2025-10-24 02:22:33 -0500

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/wayki-logistics-mvp/' // necesario para GitHub Pages
+  base: '/wayki-logistics-mvp/'   // necesario para GitHub Pages
 })
