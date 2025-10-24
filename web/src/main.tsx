76446c77ac248a4ec1e503da6c6a2351aeb1f12c
EduardoVidal87 <eduardo@wayki.co>
2025-10-24 03:15:20 -0500

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,3 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-createRoot(document.getElementById("root")!).render(<App />);
+
+const el = document.getElementById("root")!;
+createRoot(el).render(<App />);
