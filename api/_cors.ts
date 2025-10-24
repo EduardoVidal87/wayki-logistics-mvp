47de109c123a5c4bb6d36692fa493a580c18cd8f
EduardoVidal87 <eduardo@wayki.co>
2025-10-24 03:47:30 -0500

    Update _cors.ts

--- api/_cors.ts
+++ api/_cors.ts
@@ -1,7 +1,1 @@
-import type { VercelResponse } from '@vercel/node';
-
-export function withCORS(res: VercelResponse) {
-  res.setHeader('Access-Control-Allow-Origin', '*');            // en prod: pon tu dominio de Pages
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-}
+res.setHeader('Access-Control-Allow-Origin', 'https://eduardovidal87.github.io');
